b36a571943af9082f175a334ed27122326699ef3
Chito Baldedomar <cgbaldedomar@gmail.com>
2018-02-13 12:51:00 +0800

    Project created

---
+++ TestGit2/TestGit2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestGit2.Startup))]
+namespace TestGit2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
